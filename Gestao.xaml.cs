c78ca29fc69764fa9c0007cb51454528e503481e
Raphael <raphaelrodriguesssilva@gmail.com>
2023-08-22 20:56:54 -0300

    Atualizando

--- Gestao.xaml.cs
+++ Gestao.xaml.cs
@@ -26,6 +26,25 @@ namespace RegistroPonto
             loginWindow.Show();
             this.Close();
         }
+        private void CadastrarFuncionarioButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Crie um objeto Funcionario com os dados do novo funcionário
+            var novoFuncionario = new Funcionario
+            {
+                Nome = "Nome do Novo Funcionário",
+                Cargo = "Cargo do Novo Funcionário",
+                FotoPath = "Caminho da Foto"
+            };
+
+            var databaseContext = new DatabaseContext();
+            // Crie uma instância do repositório de funcionários
+            var funcionarioRepository = new FuncionarioRepository(databaseContext);
+
+            // Chame o método para cadastrar o novo funcionário
+            funcionarioRepository.CadastrarFuncionario(novoFuncionario);
+
+            // Você pode adicionar aqui uma mensagem de sucesso, atualizar a lista de funcionários exibidos na interface, etc.
+        }
         
     }
 }
