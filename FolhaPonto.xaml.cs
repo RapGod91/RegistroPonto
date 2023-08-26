fa78c22f60c898c30f2c8b17180b10f6c181aca0
Raphael <raphaelrodriguesssilva@gmail.com>
2023-08-25 23:21:43 -0300

    [FIX] Folha Ponto

--- FolhaPonto.xaml.cs
+++ FolhaPonto.xaml.cs
@@ -1,13 +1,26 @@
 using System;
+using System.Linq;
 using System.Windows;
+using RegistroPonto.Models;
+using RegistroPonto.Repositories;
 
 namespace RegistroPonto.Views
 {
     public partial class FolhaPonto : Window
     {
+        private DatabaseContext _databaseContext;
+
         public FolhaPonto()
         {
             InitializeComponent();
+            _databaseContext = new DatabaseContext();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            string nomeFuncionario = NomeFuncionarioTextBox.Text;
+            
+            
         }
 
         private void VoltarButton_Click(object sender, RoutedEventArgs e)
